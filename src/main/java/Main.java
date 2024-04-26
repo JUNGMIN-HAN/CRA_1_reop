d4eea94438e28c6c4065d50fff2d388b5b94e87d
leah <dmsrud6178@naver.com>
2024-04-26 14:02:30 +0900

    getSum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HI");
+        System.out.println("HOHO");}
+
+    public int getSum(int a, int b){
+        return a+b;
     }
 }
