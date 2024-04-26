1817186ee67589618774afa02a8ac41e2f2cacbf
jungmin <gmlsajfl93@gmail.com>
2024-04-26 14:07:34 +0900

    getZegop, getMinus 기능 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HI");
     }
+
+    public static int getZegop(int a) {
+        return a * a;
+    }
+
+    public static int getMinus(int a, int b) {
+        return a - b;
+    }
 }
