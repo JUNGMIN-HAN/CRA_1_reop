588e4136bfee8d60e725a4c723c15702fc5dc2ee
dw.koh <dw0402.koh@samsung.com>
2024-04-26 14:09:35 +0900

    나누기 함수 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HI");
     }
+
+    public int getDivide(int a, int b) {
+        if (b == 0) return 0;
+        return a/b;
+    }
 }
